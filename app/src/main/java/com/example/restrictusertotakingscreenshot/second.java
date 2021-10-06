0358fda88766226ebd5b7dc645d0c5c0b91de174
Mehedi98 <mrt.techappsfoundation@gmail.com>
2021-10-07 00:04:42 +0600

    Add new activity ,here also can't take screen sort.

--- app/src/main/java/com/example/restrictusertotakingscreenshot/second.java
+++ app/src/main/java/com/example/restrictusertotakingscreenshot/second.java
@@ -2,18 +2,16 @@ package com.example.restrictusertotakingscreenshot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class second extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,WindowManager.LayoutParams.FLAG_SECURE);
-        setContentView(R.layout.activity_main);
-        Intent intent= new Intent(MainActivity.this,second.class);
-        startActivity(intent);
+
     }
 }
